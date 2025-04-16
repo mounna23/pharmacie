22dc432555aa394bfaec5143b5fd2850235fa358
mounna23 <mouna.eddaoudi@gmail.com>
2025-04-16 19:44:13 +0000

    Initial commit

---
+++ src/main/java/com/example/backendpharmacie/BackendPharmacieApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendpharmacie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPharmacieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPharmacieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendpharmacie/BackendPharmacieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendpharmacie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPharmacieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
